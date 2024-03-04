8e6328f7ec1c803104c26ea293800413dfbf0d3f
Oliver <oliversebastian321@gmail.com>
2024-03-04 13:35:49 +0700

    border login, add profile user, add dropdown, fixing bug adminPage

--- resources/views/adminpage.blade.php
+++ resources/views/adminpage.blade.php
@@ -2,52 +2,50 @@
 
 @section('content')
 
+    <aside id="default-sidebar" class="fixed left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <span class="sr-only">Open sidebar</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            </svg>
+        </button>
+        <div class="sideNav">               
+            <div id="sidebar" class="bg-red-800 h-screen md:block shadow-xl px-3  w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300 ease-in-out" x-show="sidenav" @click.away="sidenav = false">
+                    
+                <div class="space-y-6 md:space-y-10 mt-10">
+            
+                    <div id="profile" class="space-y-3">
+                        <img src="{{ asset('images/profile.png') }}" alt="Avatar user" class="w-10 md:w-16 rounded-full mx-auto"/>
+                    <div> 
 
-<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-    <span class="sr-only">Open sidebar</span>
-    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-    </svg>
- </button>
- 
- <aside id="default-sidebar" class="fixed left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div class="sideNav">               
-        <div id="sidebar" class="bg-red-800 h-screen md:block shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300 ease-in-out" x-show="sidenav" @click.away="sidenav = false">
-                
-            <div class="space-y-6 md:space-y-10 mt-10">
-        
-                <div id="profile" class="space-y-3">
-                    <img src="{{ asset('images/profile.png') }}" alt="Avatar user" class="w-10 md:w-16 rounded-full mx-auto"/>
-                <div> 
-
-                        <h2 class="font-medium text-xs md:text-sm text-center text-white">
-                            Oliver Sebastian
-                        </h2>
-                        <p class="text-xs text-white text-center">Super Admin</p>
+                            <h2 class="font-medium text-xs md:text-sm text-center text-white">
+                                Oliver Sebastian
+                            </h2>
+                            <p class="text-xs text-white text-center">Super Admin</p>
+                            </div>
+                        </div>
+            
+                        <div id="menu" class="flex flex-col space-y-2">
+                            <a href="" class="text-sm font-medium text-white py-2 px-2 hover:bg-gray-500 hover:text-white  rounded-md transition duration-150 ease-in-out">
+                                <i class="fas fa-house" style="color: #ffffff;"></i>
+                                <span class="">Dashboard</span>
+                            </a>
+                        
+                            <a href="" class="text-sm font-medium text-white py-2 px-2 hover:bg-gray-500 hover:text-white rounded-md transition duration-150 ease-in-out">
+                                <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+                                <span class="">User Management</span>
+                            </a>
+            
+                            <a href="{{ url('/') }}" class="text-sm font-medium text-white py-2 px-2 hover:bg-gray-500 hover:text-white rounded-md transition duration-150 ease-in-out">
+                                <i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i>
+                                <span class="">Logout</span>
+                            </a>
                         </div>
                     </div>
-        
-                    <div id="menu" class="flex flex-col space-y-2">
-                        <a href="" class="text-sm font-medium text-white py-2 px-2 hover:bg-gray-500 hover:text-white  rounded-md transition duration-150 ease-in-out">
-                            <i class="fas fa-house" style="color: #ffffff;"></i>
-                            <span class="">Dashboard</span>
-                        </a>
-                     
-                        <a href="" class="text-sm font-medium text-white py-2 px-2 hover:bg-gray-500 hover:text-white rounded-md transition duration-150 ease-in-out">
-                            <i class="fa-solid fa-user" style="color: #ffffff;"></i>
-                            <span class="">User Management</span>
-                        </a>
-        
-                        <a href="{{ url('/') }}" class="text-sm font-medium text-white py-2 px-2 hover:bg-gray-500 hover:text-white rounded-md transition duration-150 ease-in-out">
-                            <i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i>
-                            <span class="">Logout</span>
-                        </a>
-                    </div>
-                </div>
-    </div> 
- </aside>
+        </div> 
+    </aside>
  
-    <div class="p-4 sm:ml-64">
+    <div class="p-4 sm:ml-64 flex-grow">
         <h1 class="text-2xl font-bold text-lack">Dashboard</h1>
 
         <div class="dropdown mt-5">
@@ -94,29 +92,6 @@
                     </div>
                 </div>
             </form>
-
-            {{-- <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center border border-transparent border-gray-300 focus:border-gray-300 focus:outline-none" type="button">
-                Select Department
-                <i class="fa-solid fa-caret-down pl-5" style="color: #000000;"></i>
-            </button>
-                
-                <!-- Dropdown menu -->
-                <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                    </ul>
-            </div> --}}
         </div>
 
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
@@ -249,7 +224,7 @@
     </div>
 
     <div class="plusButton">
-        <a href="{{ url('/upload') }}" class="fixed bottom-4 right-4">
+        <a href="#" class="fixed bottom-4 right-4">
             <div class="bg-red-800 p-5 rounded-full">
                 <i class="fa-solid fa-plus text-white"></i>
             </div>
