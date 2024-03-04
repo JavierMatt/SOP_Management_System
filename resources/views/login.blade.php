50f7b67d57fe5292afad7d4d832a3b967017dd91
BryanVK <bryanviriyak@gmail.com>
2024-03-04 13:24:47 +0700

    Update login page UI border, bugs sidenav home page clear

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,26 +2,26 @@
 
 @section('content')
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <h1 class="text-2xl font-bold text-white">Login</h1>
-        <h3 class="mt-2 text-sm text-white">Join us to unlock your documents</h3>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm bg-white p-4 opacity-90 rounded-xl">
+        <h1 class="text-2xl font-bold text-rose-800">Login</h1>
+        <h3 class="mt-2 text-sm text-rose-800">Join us to unlock your documents</h3>
     
         <form class="space-y-6" action="{{ route('login') }}" method="POST">
         @csrf
           <div>
             <div class="mt-5">
-              <input id="username" name="username" type="text" autocomplete="username" placeholder=" Enter your username" required class="block w-full rounded-md py-1.5 text-black placeholder:text-gray-400 sm:text-sm sm:leading-6">
+              <input id="username" name="username" type="text" autocomplete="username" placeholder=" Username" required class="block w-full rounded-md py-1.5 text-gray-600 p-2 border-solid border-2 border-rose-800 placeholder:text-gray-400 sm:text-sm sm:leading-6">
             </div>
           </div>
     
           <div>
             <div class="mt-2">
-                <input id="password" name="password" type="password" autocomplete="current-password" required placeholder=" Enter your password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6">
+                <input id="password" name="password" type="password" autocomplete="current-password" required placeholder="Password" class="block w-full border-solid border-2 p-2 border-rose-800 rounded-md border-0 py-1.5 text-gray-600 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6">
             </div>
           </div>
     
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-rose-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-white hover:text-rose-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-rose-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-900  hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
           </div>
         </form>
       </div>
