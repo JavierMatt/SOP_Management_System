924907c80fd2a52dc3e19ecaa560b3848c2d5484
Javier <javiermatthew0807@gmail.com>
2024-03-07 15:56:41 +0700

    final

--- resources/views/layouts/templateAdmin.blade.php
+++ resources/views/layouts/templateAdmin.blade.php
@@ -6,7 +6,7 @@
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <link rel="stylesheet" type="image/png" href="{{ asset('/css/form.css') }}">
     <title>SOP Management System</title>
-    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/form.css') }}"> -->
     @vite('resources/css/app.css')
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
 </head>   
