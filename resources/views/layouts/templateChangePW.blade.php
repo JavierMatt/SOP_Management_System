a879b02e26dfb46136534511050eb86b0471a082
BryanVK <bryanviriyak@gmail.com>
2024-03-07 15:15:00 +0700

    update background

--- resources/views/layouts/templateChangePW.blade.php
+++ resources/views/layouts/templateChangePW.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+    <link rel="stylesheet" type="image/png" href="{{ asset('css/form.css') }}">
     <title>SOP Management System</title>
     <link rel="stylesheet" href="{{ asset('css/changePW.css') }}">
     @vite('resources/css/app.css')
